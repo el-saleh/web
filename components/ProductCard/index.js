6e75ac6e1a06e72e8b9ae60f300f4484838fce23
ahmed <hussain>
2021-05-15 02:17:14 +0200

    enhance product card styling in responsive

--- components/ProductCard/index.js
+++ components/ProductCard/index.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 import styles from "./ProductCard.module.scss";
 import { IoCartOutline } from "react-icons/io5";
 
-export default function ProductCard(props) {
+export default function ProductCard() {
     // const [src, setSrc] = useState(props.productImage);
 
     // const setFallbackImage = () => {
@@ -11,22 +11,24 @@ export default function ProductCard(props) {
     // }
 
     return (
-        <div className={styles.ProductCard}>
-            <Link href="/product/great_new_product">
-                <a>
-                    <div className={styles.imgContainer}>
-                        <img src={`/assets/products/prod${Math.ceil(Math.random() * 5)}.jpg`} alt={""} />
-                    </div>
-                </a>
-            </Link>
-            <div className={styles.infoContainer}>
+        <div className={styles.productCard_Wrapper}>
+            <div className={styles.ProductCard}>
                 <Link href="/product/great_new_product">
                     <a>
-                        <h5 className={styles.name}>{"عنوان تفصيلي واضح للمنتج المعروض"}</h5>
+                        <div className={styles.imgContainer}>
+                            <img src={`/assets/products/prod${Math.ceil(Math.random() * 5)}.jpg`} alt={""} />
+                        </div>
                     </a>
                 </Link>
-                <p className={styles.price}>{(Math.ceil((Math.random()*10))*100).toFixed(2)} جنيه</p>
-                <button className={styles.addButton}>{"أضف إلى العربة "} &nbsp;<IoCartOutline /></button>
+                <div className={styles.infoContainer}>
+                    <Link href="/product/great_new_product">
+                        <a>
+                            <h5 className={styles.name}>{"عنوان تفصيلي واضح للمنتج المعروض"}</h5>
+                        </a>
+                    </Link>
+                    <p className={styles.price}>{(Math.ceil((Math.random()*10))*100).toFixed(2)} جنيه</p>
+                    <button className={styles.addButton}>{"أضف إلى العربة "} &nbsp;<IoCartOutline /></button>
+                </div>
             </div>
         </div>
     )
