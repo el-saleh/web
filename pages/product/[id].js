51964782aec9686e3b4508c0eaf3e22ea123bb60
Ahmed Hussain <ah0240089>
2021-07-14 12:27:57 +0200

    parse description text from raw html description in product page

--- pages/product/[id].js
+++ pages/product/[id].js
@@ -8,24 +8,29 @@ import dummy from "../../utilities/dummy";
 
 function product(props) {
   const router = useRouter()
-  const { id } = router.query
+  const { id } = router.query;
+
+  let descriptionDiv = document.createElement('div');
+  descriptionDiv.innerHTML=props.description;
+  
+  let descriptionText = descriptionDiv.innerText;
 
   return (
     <>
       <Head>
         <title>{`elsaleh | ${props.title}`}</title>
-        <meta name="description" content={props.description} />
+        <meta name="description" content={descriptionText} />
 
         {/* Open Graph / Facebook */}
         <meta property="og:type" content="product" />
         <meta property="og:url" content={`https://www.elsaleh.net/product/${id}`} />
-        <meta property="og:description" content={props.description} />
+        <meta property="og:description" content={descriptionText} />
         <meta property="og:image" content={props.productImage?.imageUrl} />
 
         {/* Twitter */}
         <meta property="twitter:card" content="summary_large_image" />
         <meta property="twitter:url" content={`https://www.elsaleh.net/product/${id}`} />
-        <meta property="twitter:description" content={props.description} />
+        <meta property="twitter:description" content={descriptionText} />
         <meta property="twitter:image" content={props.productImage?.imageUrl} />
       </Head>
       <Layout>
